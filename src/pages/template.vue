db466970bb5d17325263921614e8ffe67d79d888
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-04-08 19:27:58 +0800

    Create templates

---
+++ src/pages/template.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="template">
+        <div class="buttons">
+            <h1>Buttons</h1>
+            <ButtonRound message="Round Button" />
+        </div>
+    </div>
+</template>
+
+<script>
+import ButtonRound from '../components/button-round.vue';
+
+export default {
+    name: 'Template',
+
+    components : {
+        ButtonRound
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
