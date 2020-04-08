7ee7a84b17a3d5c78e42ddb03752bb6364aca2aa
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-04-08 19:27:43 +0800

    Create Button

---
+++ src/components/button-round.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    <v-btn rounded color="primary" dark> {{ message }} </v-btn>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: ButtonRound,
+        props: {
+            message : String
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
