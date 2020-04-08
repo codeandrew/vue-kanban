69a1871744c5d04fcadcc7730de0c3bdb2c2d654
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-04-08 18:52:47 +0800

    Create Dashboard

---
+++ src/pages/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <section id="dashboard">
+        <div class="container">
+            <h1>{{ title }} </h1>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Dashboard',
+        props:{
+            title : String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
