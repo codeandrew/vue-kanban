82fcfaeb80d55d8d0c42e7fb63a7303e378af647
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-04-08 18:53:04 +0800

    Import dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Dashboard title="Dashboard"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './pages/dashboard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Dashboard
   }
 }
 </script>
